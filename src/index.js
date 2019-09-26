73dacd89266fbcb9eab315a2147425a82d2f9d8f
2reek <mountagest@gmail.com>
2019-09-26 12:31:53 +0300

    correct index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-     // Запускаем функцыю с рекурсией и вырезаем пары скобок идущие подряд.
-    function counter(str, bracketsConfig) {                         
-      let strBrackets = str;                          //Присваиваем иходное значение скобок
-      for (i = 0; i < bracketsConfig.length; i++) {   //Перебираем все скобки Config
-        let brackets = bracketsConfig[i].join('');    //Находим пару и удаляем
-        while (~str.indexOf(brackets)) {          
-          str = str.replace(brackets, '');            //Возвращаем измененную строку
-        }
+  function counter(str, bracketsConfig) {
+    const stringBrackets = str;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let brackets = bracketsConfig[i].join("");
+      while (~str.indexOf(brackets)) {
+        str = str.replace(brackets, "");
       }
-      if (str != strBrackets ) {                      //Проверяем изменилась ли строка посде цикла удаления скобок
-        return counter(str, bracketsConfig);          //Если изменилась, то запускае цикл еще раз
-      }
-      return str;                                     //Возвращаем конечную строку
     }
 
-    let newStr = counter(str, bracketsConfig);         
-      if (newStr == 0) {                              
-       return true;                                   //Если строка пустая то TRUE
+    if (str !== stringBrackets) {
+      return counter(str, bracketsConfig);
     }
-    return false;                                     //Иначе FALCE
+    return str;
+  }
 
-}
+  if (counter(str, bracketsConfig) == 0) {
+    return true;
+  }
+  return false;
+};
